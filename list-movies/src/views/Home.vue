a07cacc5386816d24ba0626368d4f6ef2870d8e0
alvarengajr1 <malvarengajr@gmail.com>
2020-06-04 16:30:46 -0400

    o inicio de um sonho

---
+++ list-movies/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+      <h1> Home </h1>
+      <p> o começo de um sonho</p>
+  <div id="login">
+    Login 
+
+    
+  </div>
+  </div>
+
+  
+</template>
+<style>
+  #login {
+    
+   padding: 4em;
+    border-radius: 40px;
+    margin-top: 5em;
+    margin-right: 9em;
+    margin-left: 9em;
+    
+    line-height: 4em;
+    box-shadow: 5px 5px 15px rgba(204, 204, 204);
+}
+</style>
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
