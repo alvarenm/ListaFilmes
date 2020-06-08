d105e4bf1572d0c443477224eeb398cf05a562cc
alvarengajr1 <malvarengajr@gmail.com>
2020-06-07 21:21:47 -0400

    ajeitando login

--- list-movies/src/DB/database.js
+++ list-movies/src/DB/database.js
@@ -1,5 +1,5 @@
 var contas = [
-    {email: 'batata@gmail.com', senha: 'abacate123', nome: 'Marcelo Alvarenga Junior', nascimento: '28/02/1999', listaFilmes: [{}], login: true, id: 1, listaAssistidos: [{}],}
+    {email: 'batata@gmail.com', senha: 'abacate123', nome: 'Marcelo Alvarenga Junior', nascimento: '28/02/1999', listaFilmes: [{}], login: false, id: 1, listaAssistidos: [{}],}
 ]
 
 export default {
